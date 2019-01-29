cdfbabf0ccbcd7ca84444bbad45870567ae01745
muradqasaimeh <qasaimeh@iastate.edu>
2019-01-28 18:07:35 -0600

    add GPU tests

---
+++ GPUTests/Image Analysis/testMinMaxLoc/src/testGPUMinMaxLoc.cpp
@@ -0,0 +1,250 @@
+/*****************************************************************************
+*
+* 	Copyright (c) 2019 Iowa State University
+*
+* 	Redistribution and use in source and binary forms, with or without
+* 	modification, are permitted provided that the following conditions 
+* 	are met:
+*
+* 	1.Redistributions of source code must retain the above copyright 
+* 	notice, this list of conditions and the following disclaimer.
+* 	2.Redistributions in binary form must reproduce the above copyright 
+* 	notice, this list of conditions and the following disclaimer in the
+* 	documentation and/or other materials provided with the distribution.
+* 	3.Neither the name of the copyright holder nor the names of its 
+* 	contributors may be used to endorse or promote products derived 
+* 	from this software without specific prior written permission.
+*
+* 	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+* 	"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+* 	LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+* 	FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+*	COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+* 	INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+* 	BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+* 	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+* 	CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+* 	LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+* 	ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+* 	POSSIBILITY OF SUCH DAMAGE.
+*
+*	(c) Copyright 2019 Iowa State University.
+*	All rights reserved.
+*	
+*	Author: Murad Qasaimeh <qasaimeh@iastate.edu>
+*	Date:   2018/03/27
+*****************************************************************************/
+
+
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <opencv2/opencv.hpp>
+#include "opencv2/core.hpp"
+#include <opencv2/highgui/highgui.hpp>
+#include "OpenCVUtils.h" 
+#include "opencv2/core/cuda.hpp" 
+#include "ivx.hpp"     
+#include <HRTimer.h>
+
+int main(int argc, char** argv)
+{
+	HRTimer timer;
+
+	try {	
+		const cv::String keys =
+			"{help h usage ? |                 | print this message                   }"
+			"{@image         |                 | input image 	                      }"
+			"{goldenFile gf  |                 | golden output image (from SW)        }"
+			"{outFile of     |                 | output image (from PL)               }"
+			"{display d      |                 | diplay result with imshow            }"
+			"{iterations n   | 1               | number of iterations to measure time }"
+			;
+
+		cv::CommandLineParser parser(argc, argv, keys.c_str());
+		if (parser.has("help") || argc < 2)
+		{
+			parser.printMessage();
+			std::cout << "\nhit enter to quit...";
+			std::cin.get();
+			return 0;
+		}
+
+		cv::String filenameIn = parser.get<cv::String>(0);
+
+		cv::String filenameSW; bool writeSWResult = false;
+		if (parser.has("goldenFile")) {
+			filenameSW = parser.get<cv::String>("goldenFile");
+			writeSWResult = true;
+		}
+
+		cv::String filenamePL; bool writePLResult = false;
+		if (parser.has("outFile")) {
+			filenamePL = parser.get<cv::String>("outFile");
+			writePLResult = true;
+		}
+
+		bool imShowOn = parser.has("display");
+		unsigned int numberOfIterations;
+		if (parser.has("iterations"))
+			numberOfIterations = parser.get<unsigned int>("iterations");
+
+		if (!parser.check())
+		{
+			parser.printErrors();
+			return(-1);
+		}
+
+		// Initialize
+		cv::Mat srcIn;
+		double minSW, maxSW;
+		cv::Point minLocSW(-1,-1), maxLocSW(-1,-1);
+
+		initializeSingleGrayImageTest(filenameIn, srcIn);
+
+		int width = srcIn.size().width;
+		int height = srcIn.size().height;
+   
+		std::cout << "running on CPU" << std::endl;
+	 
+		//warming up
+		cv::minMaxLoc(srcIn, &minSW, &maxSW, &minLocSW, &maxLocSW); 				
+		timer.StartTimer();
+		for (int i = 0; i < numberOfIterations; i++) 
+		{
+			cv::minMaxLoc(srcIn, &minSW, &maxSW, &minLocSW, &maxLocSW);
+		}
+		timer.StopTimer(); 		
+		std::cout << "Elapsed time over " << numberOfIterations << "SW call(s): " << timer.GetElapsedUs() << " us or " << (float)timer.GetElapsedUs() / (float)numberOfIterations << "us per frame" << std::endl;
+
+ 	
+		/*-------------------------- CUDA ---------------------------------------------*/
+		bool ranCuda = false;
+		double minCuda, maxCuda;
+		cv::Point minLocCuda(-1,-1), maxLocCuda(-1,-1);	
+#ifdef WITH_CUDA
+		
+		std::cout << "CUDA gpu device count: " << cv::cuda::getCudaEnabledDeviceCount() << std::endl;
+		if (cv::cuda::getCudaEnabledDeviceCount() < 1) {
+			std::cout << "no Cuda devices available" << std::endl;
+		}
+		else
+		{	
+			cv::cuda::printShortCudaDeviceInfo(cv::cuda::getDevice());
+	
+			cv::cuda::GpuMat srcCuda;	
+
+
+			cv::cuda::Stream stream;
+			srcCuda.upload(srcIn);
+
+			std::cout << "running CUDA" << std::endl;
+    		 
+			//warming up
+			cv::cuda::minMaxLoc(srcCuda, &minCuda, &maxCuda, &minLocCuda, &maxLocCuda); 				
+			timer.StartTimer();
+			for (int i = 0; i < numberOfIterations; i++) 
+			{
+				cv::cuda::minMaxLoc(srcCuda, &minCuda, &maxCuda, &minLocCuda, &maxLocCuda);
+			}  
+			timer.StopTimer();  		
+			std::cout << "Elapsed time over " << numberOfIterations << "CUDA call(s): " << timer.GetElapsedUs() << " us or " << (float)timer.GetElapsedUs() / (float)numberOfIterations << "us per frame" << std::endl;
+			ranCuda = true;
+		}
+#endif
+
+		/*-------------------------- OPENVX ---------------------------------------------*/
+		bool ranOpenvx = false;   
+		uint8_t minOpenVX, maxOpenVX;
+		//cv::Mat minLocOpenVX, maxLocOpenVX;
+		vx_uint32 minLocOpenVX[3];
+		vx_uint32 maxLocOpenVX[3];
+#ifdef WITH_OPENVX
+
+	 	//create context
+		ivx::Context context = ivx::Context::create();
+
+		//create src image
+        ivx::Image ivxImage = ivx::Image::create(context, width, height,  VX_DF_IMAGE_U8);
+
+		//copy cv::Mat to vx_image   
+        ivxImage.copyFrom(0, srcIn);    
+    
+		ivx::Scalar minVal= ivx::Scalar::create<VX_TYPE_UINT8>(context, 0.0);		
+		ivx::Scalar maxVal= ivx::Scalar::create<VX_TYPE_UINT8>(context, 0.0);  
+		ivx::Scalar minCount= ivx::Scalar::create< VX_TYPE_UINT32>(context, 0.0);		
+		ivx::Scalar maxCount= ivx::Scalar::create< VX_TYPE_UINT32>(context, 0.0); 
+
+ 		ivx::Array minLoc= ivx::Array::create(context,VX_TYPE_COORDINATES2D, 3);  		
+		ivx::Array maxLoc= ivx::Array::create(context,VX_TYPE_COORDINATES2D, 3);  
+
+		//create graph
+ 		ivx::Graph graph = ivx::Graph::create(context);  
+		ivx::Node::create(graph, VX_KERNEL_MINMAXLOC, ivxImage, minVal, maxVal, minLoc, maxLoc, minCount, maxCount);  
+		
+		//verify graph
+		graph.verify();  
+ 
+		std::cout << "running OpenVX" << std::endl;			
+		
+		//warming up
+ 		graph.process();   			
+		timer.StartTimer();
+		for (int i = 0; i < numberOfIterations; i++){
+ 			graph.process(); 
+		}
+		timer.StopTimer();  			
+		minVal.getValue(minOpenVX);
+  	 	maxVal.getValue(maxOpenVX);
+		minLoc.copyTo(&minLocOpenVX);
+  	 	maxLoc.copyTo(&maxLocOpenVX);
+
+		std::cout << "Elapsed time over " << numberOfIterations << " OpenVX call(s): " << timer.GetElapsedUs() << " us or " << (float)timer.GetElapsedUs() / (float)numberOfIterations << "us per frame" << std::endl;
+
+		ranOpenvx = true;
+#endif
+
+		/*------------- end of measurements ----------------------*/
+		
+ 		
+		//show results
+	 	if (imShowOn) {
+			cv::imshow("Input image", srcIn); 
+			cv::waitKey(0); 
+				std::cout<<"minSW = "<< minSW <<" @ "<<minLocSW<< std::endl;
+				std::cout<<"maxSW = "<< maxSW <<" @ "<<maxLocSW<< std::endl; 
+				if (ranCuda)
+				{
+					std::cout<<"minCUDA = "<< minCuda <<" @ "<<minLocCuda<< std::endl;
+					std::cout<<"maxCUDA = "<< maxCuda <<" @ "<<maxLocCuda<< std::endl;
+				}
+				if (ranOpenvx)
+				{
+					std::cout<<"minOpenVX = "<< (int)minOpenVX <<" @ "<<minLocOpenVX[0]<<" "<<minLocOpenVX[1]<< std::endl;
+					std::cout<<"maxOpenVX = "<< (int)maxOpenVX <<" @ "<<maxLocOpenVX[0]<<" "<<maxLocOpenVX[1]<<  std::endl;
+				}
+
+				std::cout<<"\n";
+			 
+		} 
+	}
+	catch (std::exception &e)
+	{
+		const char* errorMessage = e.what();
+		std::cerr << "Exception caught: " << errorMessage << std::endl;
+		std::cout << "\nhit enter to quit...";
+		std::cin.get();
+		exit(-1);
+	}
+	catch (const char *e) {
+		std::cerr << "Exception caught: " << e << std::endl;
+		std::cout << "\nhit enter to quit...";
+		std::cin.get();
+		exit(-1);
+	}
+
+	std::cout << "\nhit enter to quit...";
+	std::cin.get();
+	return 0;
+}
+ 
